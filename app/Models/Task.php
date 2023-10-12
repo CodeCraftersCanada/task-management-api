819e10824c24cfe208bd9ec44dc9f2045e2b79de
Anna Lou Parejo <aparejo0984@gmail.com>
2023-10-12 08:37:25 -0400

    Add SubTask table and hourly rate on task table

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -18,6 +18,7 @@ class Task extends Model
         'task_status_id',
         'created_by',
         'assigned_to',
+        'hours_to_complete'
     ];
 
     public function status()
--- database/migrations/2023_10_12_122118_create_sub_task_table.php
+++ database/migrations/2023_10_12_122118_create_sub_task_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasks extends Migration
+class CreateSubTaskTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,26 +13,27 @@ class CreateTasks extends Migration
      */
     public function up()
     {
-        Schema::create('task', function (Blueprint $table) {
+        Schema::create('sub_task', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100);
             $table->text('description');
+            $table->bigInteger('task_id')->unsigned();
             $table->bigInteger('task_status_id')->unsigned();
-            $table->decimal('task_hours', 8, 2)->nullable();
             $table->bigInteger('created_by')->unsigned();
             $table->bigInteger('assigned_to')->unsigned();
             $table->timestamp('start_date')->nullable();
             $table->timestamp('end_date')->nullable();
+            $table->decimal('task_hours', 8, 2)->nullable();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
 
-        Schema::table('task', function($table) {
+        Schema::table('sub_task', function($table) {
             $table->foreign('task_status_id')->references('id')->on('task_status');
             $table->foreign('created_by')->references('id')->on('users');
             $table->foreign('assigned_to')->references('id')->on('users');
+            $table->foreign('task_id')->references('id')->on('task');
         });
-
     }
 
     /**
@@ -42,6 +43,6 @@ class CreateTasks extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('sub_task');
     }
 }
