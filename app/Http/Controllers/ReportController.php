6d9b7174b5b30d6ae8ae61b2579fa94b7b6b4320
Anna Lou Parejo <aparejo0984@gmail.com>
2023-10-18 16:34:52 -0400

    Fix Reports per member and admin

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -15,6 +15,9 @@ class ReportController extends Controller
      */
     public function index(Request $request)
     {
+        //get the current users
+        $user = auth('sanctum')->user();
+
         $now = date("Y-m-d G:i:s");
         $lastWeek = date("Y-m-d G:i:s", strtotime("-1 week")); // Subtract a week from the current date
 
@@ -23,30 +26,63 @@ class ReportController extends Controller
         $query->whereBetween('created_at', [$lastWeek, $now]);
 
         $reports = [];
-        $reports['total'] = $query->count();
-        $reports['overall_new'] = DB::table('task')->where('task_status_id', '=', 1)->count();
-        $reports['overall_inprogress'] = DB::table('task')->where('task_status_id', '=', 2)->count();
-        $reports['overall_complete'] = DB::table('task')->where('task_status_id', '=', 3)->count();
-        $reports['new_tasks_week'] = DB::table('task')->whereBetween('created_at', [$lastWeek, $now])->where('task_status_id', '=', 1)->count();
-        $reports['inprogress_tasks_week'] = DB::table('task')->whereBetween('created_at', [$lastWeek, $now])->where('task_status_id', '=', 2)->count();
-        $reports['completed_tasks_week'] = DB::table('task')->whereBetween('created_at', [$lastWeek, $now])->where('task_status_id', '=', 3)->count();
-
         $months = [];
 
-        $months[1] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-01-01 00:00:00', '2023-01-31 23:23:23'])->count();
-        $months[2] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-02-01 00:00:00', '2023-02-31 23:23:23'])->count();
-        $months[3] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-03-01 00:00:00', '2023-03-31 23:23:23'])->count();
-        $months[4] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-04-01 00:00:00', '2023-04-31 23:23:23'])->count();
-        $months[5] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-05-01 00:00:00', '2023-05-31 23:23:23'])->count();
-        $months[6] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-06-01 00:00:00', '2023-06-31 23:23:23'])->count();
-        $months[7] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-07-01 00:00:00', '2023-07-31 23:23:23'])->count();
-        $months[8] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-08-01 00:00:00', '2023-08-31 23:23:23'])->count();
-        $months[9] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-09-01 00:00:00', '2023-09-31 23:23:23'])->count();
-        $months[10] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-10-01 00:00:00', '2023-10-31 23:23:23'])->count();
-        $months[11] = DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-11-01 00:00:00', '2023-11-31 23:23:23'])->count();
-        $months[12] =  DB::table('task')->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-12-01 00:00:00', '2023-12-31 23:23:23'])->count();
+        //admin - created_by
+        if ($user->user_type_id == 1) {
+            $reports['total'] = $query->where('created_by', '=', $user->id)->count();
+            $reports['overall_new'] = DB::table('task')->where('task_status_id', '=', 1)->where('created_by', '=', $user->id)->count();
 
-        $reports['completed_tasks_monthly'] = $months;
+            $reports['total'] = $query->where('created_by', '=', $user->id)->count();
+            $reports['overall_new'] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 1)->count();
+            $reports['overall_inprogress'] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 2)->count();
+            $reports['overall_complete'] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->count();
+            $reports['new_tasks_week'] = DB::table('task')->where('created_by', '=', $user->id)->whereBetween('created_at', [$lastWeek, $now])->where('task_status_id', '=', 1)->count();
+            $reports['inprogress_tasks_week'] = DB::table('task')->where('created_by', '=', $user->id)->whereBetween('created_at', [$lastWeek, $now])->where('task_status_id', '=', 2)->count();
+            $reports['completed_tasks_week'] = DB::table('task')->where('created_by', '=', $user->id)->whereBetween('created_at', [$lastWeek, $now])->where('task_status_id', '=', 3)->count();
+
+            $months[1] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-01-01 00:00:00', '2023-01-31 23:23:23'])->count();
+            $months[2] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-02-01 00:00:00', '2023-02-31 23:23:23'])->count();
+            $months[3] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-03-01 00:00:00', '2023-03-31 23:23:23'])->count();
+            $months[4] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-04-01 00:00:00', '2023-04-31 23:23:23'])->count();
+            $months[5] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-05-01 00:00:00', '2023-05-31 23:23:23'])->count();
+            $months[6] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-06-01 00:00:00', '2023-06-31 23:23:23'])->count();
+            $months[7] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-07-01 00:00:00', '2023-07-31 23:23:23'])->count();
+            $months[8] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-08-01 00:00:00', '2023-08-31 23:23:23'])->count();
+            $months[9] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-09-01 00:00:00', '2023-09-31 23:23:23'])->count();
+            $months[10] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-10-01 00:00:00', '2023-10-31 23:23:23'])->count();
+            $months[11] = DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-11-01 00:00:00', '2023-11-31 23:23:23'])->count();
+            $months[12] =  DB::table('task')->where('created_by', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-12-01 00:00:00', '2023-12-31 23:23:23'])->count();
+
+            $reports['completed_tasks_monthly'] = $months;
+        } else {
+            //member -
+            $reports['total'] = $query->where('assigned_to', '=', $user->id)->count();
+            $reports['overall_new'] = DB::table('task')->where('task_status_id', '=', 1)->where('assigned_to', '=', $user->id)->count();
+
+            $reports['total'] = $query->where('assigned_to', '=', $user->id)->count();
+            $reports['overall_new'] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 1)->count();
+            $reports['overall_inprogress'] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 2)->count();
+            $reports['overall_complete'] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->count();
+            $reports['new_tasks_week'] = DB::table('task')->where('assigned_to', '=', $user->id)->whereBetween('created_at', [$lastWeek, $now])->where('task_status_id', '=', 1)->count();
+            $reports['inprogress_tasks_week'] = DB::table('task')->where('assigned_to', '=', $user->id)->whereBetween('created_at', [$lastWeek, $now])->where('task_status_id', '=', 2)->count();
+            $reports['completed_tasks_week'] = DB::table('task')->where('assigned_to', '=', $user->id)->whereBetween('created_at', [$lastWeek, $now])->where('task_status_id', '=', 3)->count();
+
+            $months[1] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-01-01 00:00:00', '2023-01-31 23:23:23'])->count();
+            $months[2] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-02-01 00:00:00', '2023-02-31 23:23:23'])->count();
+            $months[3] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-03-01 00:00:00', '2023-03-31 23:23:23'])->count();
+            $months[4] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-04-01 00:00:00', '2023-04-31 23:23:23'])->count();
+            $months[5] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-05-01 00:00:00', '2023-05-31 23:23:23'])->count();
+            $months[6] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-06-01 00:00:00', '2023-06-31 23:23:23'])->count();
+            $months[7] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-07-01 00:00:00', '2023-07-31 23:23:23'])->count();
+            $months[8] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-08-01 00:00:00', '2023-08-31 23:23:23'])->count();
+            $months[9] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-09-01 00:00:00', '2023-09-31 23:23:23'])->count();
+            $months[10] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-10-01 00:00:00', '2023-10-31 23:23:23'])->count();
+            $months[11] = DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-11-01 00:00:00', '2023-11-31 23:23:23'])->count();
+            $months[12] =  DB::table('task')->where('assigned_to', '=', $user->id)->where('task_status_id', '=', 3)->whereBetween('created_at', ['2023-12-01 00:00:00', '2023-12-31 23:23:23'])->count();
+
+            $reports['completed_tasks_monthly'] = $months;
+        }
 
         $results = $query->count();
         return response()->json([
