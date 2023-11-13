9f80021222390342c985c8e8f5b74526ba1c39c8
Anna Lou Parejo <aparejo0984@gmail.com>
2023-11-12 20:46:44 -0500

    Add a dummy image for user create

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -42,7 +42,8 @@ class AuthController extends Controller
                 'user_type_id' => $request->user_type_id,
                 'user_role_id' => $request->user_role_id,
                 'hourly_rate' => $request->hourly_rate,
-                'password' => bcrypt($request->password)
+                'password' => bcrypt($request->password),
+                'filename' => "avatar-icon.jpeg"
             ]);
 
 
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,7 +65,8 @@ class UserController extends Controller
                 'user_type_id' => $request->user_type_id,
                 'user_role_id' => $request->user_role_id,
                 'hourly_rate' => $request->hourly_rate,
-                'password' => bcrypt($request->password)
+                'password' => bcrypt($request->password),
+                'filename' => "avatar-icon.jpeg"
             ]);
 
 
